b31eb174c53b2608ef247fbdecdaec44c9547026
Anmol Gupta <anmolgupta520@gmail.com>
2026-04-04 22:28:02 +0530

    feat: connect to WiFi

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,41 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <WiFi.h>
 
+#include <secrets.h>
+
+// NETWORK
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
+void initWiFi() {
+  WiFi.mode(WIFI_MODE_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println(WiFi.localIP());
+  Serial.print("RRSI: ");
+  Serial.println(WiFi.RSSI());
+}
+
+
+// OLED
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup()
 {
-  Wire.begin(21, 22);
+  Serial.begin(115200);
 
+  initWiFi();
+
+  Wire.begin(21, 22);
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
   {
     Serial.println("SSD1306 init failed");
